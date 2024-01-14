aa981a3847eb609a17a270df0a55e5be374a47ac
R4 Cheng <karaburi2023@gmail.com>
2024-01-14 19:46:00 +0800

    Refactor parking fee calculation tests

--- parking_calc/parking_free_calc_test.ts
+++ parking_calc/parking_free_calc_test.ts
@@ -1,46 +1,47 @@
 import ParkingFeeCalculator from "./parking_free_calc.ts";
 import { assertEquals } from "std/assert/assert_equals.ts";
-import { beforeAll } from "std/testing/bdd.ts";
 
-let sut: ParkingFeeCalculator;
 let start: Date;
 let end: Date;
 let actual: number;
 
-beforeAll(() => {
-  sut = new ParkingFeeCalculator();
-});
+const sut = new ParkingFeeCalculator();
+
 
 Deno.test("15 mins free", () => {
-  newFunction("2020-01-01T00:00:00", "2020-01-01T00:14:59", 0);
+  startParkingAt("2020-01-01T00:00:00");
+  endParkingAt("2020-01-01T00:14:59");
+  calculate();
+  shouldPay(0);
 });
 
 Deno.test("over 15 min not free", () => {
-  newFunction("2020-01-01T00:00:00", "2020-01-01T00:15:00", 30);
+  startParkingAt("2020-01-01T00:00:00");
+  endParkingAt("2020-01-01T00:15:00");
+  calculate();
+  shouldPay(30);
 });
 
 Deno.test("over 30 min then pay 60", () => {
-  newFunction("2020-01-01T00:00:00", "2020-01-01T00:30:01", 60);
+  startParkingAt("2020-01-01T00:00:00");
+  endParkingAt("2020-01-01T00:30:01");
+  calculate();
+  shouldPay(60);
 });
 
 Deno.test("over 60 min then pay 90", () => {
-  newFunction("2020-01-01T00:00:00", "2020-01-01T01:00:00", 90);
+  startParkingAt("2020-01-01T00:00:00");
+  endParkingAt("2020-01-01T01:00:00");
+  calculate();
+  shouldPay(90);
 });
 
 Deno.test("over 150 min then pay 150", () => {
-  newFunction("2020-01-01T00:00:00", "2020-01-01T02:30:00", 150);
-});
-
-function newFunction(startText: string, endText: string, expected: number) {
-
-  // use local datetime
-  startParkingAt(startText);
-  endParkingAt(endText);
+  startParkingAt("2020-01-01T00:00:00");
+  endParkingAt("2020-01-01T02:30:00");
   calculate();
-  
-  shouldPay(expected);
-}
-
+  shouldPay(150);
+});
 
 function shouldPay(expected: number) {
   assertEquals(actual, expected);
