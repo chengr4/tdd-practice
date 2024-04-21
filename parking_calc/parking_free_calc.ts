8b0d99d4a97975ab986fa189ad773b784651d79d
R4 Cheng <karaburi2023@gmail.com>
2024-04-21 11:53:40 +0800

    separate domain behavior

--- parking_calc/parking_free_calc.ts
+++ parking_calc/parking_free_calc.ts
@@ -7,25 +7,28 @@ class ParkingFeeCalculator {
     if (minutes <= 15) {
       return 0;
     }
-    
+
     let totalFee = 0;
     let todayStartTime: number = getStartOfDay(start).getTime();
+    const dailyDurationList: number[] = [];
 
-    // currently, it contains two domain knowledge:
-    // calculate daily duration => parking behavior
-    // calculate fee with daily duration => charging behavior
+    // The first loop is now only for parking behavior
     while (todayStartTime < end.getTime()) {
       const tomorrowStartTime: number = todayStartTime + 24 * 60 * 60 * 1000;
       const currentStart = start.getTime() > todayStartTime ?
-        start.getTime(): todayStartTime;
+        start.getTime() : todayStartTime;
       const currentEnd = end.getTime() < tomorrowStartTime ?
         end.getTime() : tomorrowStartTime;
       const sessionMinutes = (currentEnd - currentStart) / 1000 / 60;
-
-      totalFee += Math.min(this.getRegularFee(sessionMinutes), 150);
+      dailyDurationList.push(sessionMinutes);
 
       todayStartTime = tomorrowStartTime;
     }
+
+    // the second loop is for charging behavior
+    dailyDurationList.forEach((daily) => {
+      totalFee += Math.min(this.getRegularFee(daily), 150);
+    });
 
     return totalFee;
   }
