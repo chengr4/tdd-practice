49d9e4062c4f48a9e17d02f9f931069f5ad8575a
R4 Cheng <karaburi2023@gmail.com>
2024-04-21 12:40:35 +0800

    move method to its domain

--- parking_calc/parking_free_calc.ts
+++ parking_calc/parking_free_calc.ts
@@ -1,4 +1,3 @@
-import { getStartOfDay } from "./utils/date_tools.ts";
 import ParkingSession from "./parking_session.ts";
 
 class ParkingFeeCalculator {
@@ -9,7 +8,7 @@ class ParkingFeeCalculator {
       return 0;
     }
 
-    const dailyDurationList: number[] = this.getDailyDurationList(parkingSession);
+    const dailyDurationList: number[] = parkingSession.getDailyDurationList(parkingSession);
 
     let totalFee = 0;
     // the second loop is for charging behavior
@@ -18,25 +17,6 @@ class ParkingFeeCalculator {
     });
 
     return totalFee;
-  }
-
-  private getDailyDurationList(parkingSession: ParkingSession) {
-    const dailyDurationList: number[] = [];
-    let todayStartTime: number = getStartOfDay(parkingSession.getStart()).getTime();
-
-    // The first loop is now only for parking behavior
-    while (todayStartTime < parkingSession.getEnd().getTime()) {
-      const tomorrowStartTime: number = todayStartTime + 24 * 60 * 60 * 1000;
-      const currentStart = parkingSession.getStart().getTime() > todayStartTime ?
-        parkingSession.getStart().getTime() : todayStartTime;
-      const currentEnd = parkingSession.getEnd().getTime() < tomorrowStartTime ?
-        parkingSession.getEnd().getTime() : tomorrowStartTime;
-      const sessionMinutes = (currentEnd - currentStart) / 1000 / 60;
-      dailyDurationList.push(sessionMinutes);
-
-      todayStartTime = tomorrowStartTime;
-    }
-    return dailyDurationList;
   }
 
   private getRegularFee(minutes: number) {
--- parking_calc/parking_session.ts
+++ parking_calc/parking_session.ts
@@ -1,3 +1,5 @@
+import { getStartOfDay } from "./utils/date_tools.ts";
+
 class ParkingSession {
   private start: Date;
   private end: Date;
@@ -21,6 +23,25 @@ class ParkingSession {
 
   setEnd(end: Date) {
     this.end = end;
+  }
+
+   getDailyDurationList(parkingSession: ParkingSession) {
+    const dailyDurationList: number[] = [];
+    let todayStartTime: number = getStartOfDay(parkingSession.getStart()).getTime();
+
+    // The first loop is now only for parking behavior
+    while (todayStartTime < parkingSession.getEnd().getTime()) {
+      const tomorrowStartTime: number = todayStartTime + 24 * 60 * 60 * 1000;
+      const currentStart = parkingSession.getStart().getTime() > todayStartTime ?
+        parkingSession.getStart().getTime() : todayStartTime;
+      const currentEnd = parkingSession.getEnd().getTime() < tomorrowStartTime ?
+        parkingSession.getEnd().getTime() : tomorrowStartTime;
+      const sessionMinutes = (currentEnd - currentStart) / 1000 / 60;
+      dailyDurationList.push(sessionMinutes);
+
+      todayStartTime = tomorrowStartTime;
+    }
+    return dailyDurationList;
   }
 }
 
