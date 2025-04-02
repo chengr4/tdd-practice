2a0295f9a1b3252189ee5274fdfd9fbd5f78a5b2
R4 Cheng <karaburi2023@gmail.com>
2025-04-01 18:59:02 -0700

    make collection a temp variable

--- kotlin/test/gameserver/GetParkingFeeControllerTest.kt
+++ kotlin/test/gameserver/GetParkingFeeControllerTest.kt
@@ -8,8 +8,6 @@ import java.time.LocalDateTime
 class GetParkingFeeControllerTest {
     // Declare the field for the system under test (sut)
     private lateinit var sut: GetParkingFeeController
-    // Declare a field for the parking sessions data source.
-    private lateinit var parkingSessions: MutableList<ParkingSession>
     private lateinit var parkingSessionRepository: ParkingSessionRepository
 
 
@@ -17,9 +15,8 @@ class GetParkingFeeControllerTest {
     // so we use it to initialize our database (mock) and sut.
     @BeforeEach
     fun setup() {
-        // Initialize the mock database
-        parkingSessions = mutableListOf()
-        parkingSessionRepository = ParkingSessionRepository(parkingSessions)
+        // inject the mock database
+        parkingSessionRepository = ParkingSessionRepository(mutableListOf())
         // inject repository
         sut = GetParkingFeeController(parkingSessionRepository)
     }
